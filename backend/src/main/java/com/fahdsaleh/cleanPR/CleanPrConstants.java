515afe95b6b0b5ee8f896f704707a9b03f2e087f
Fahd <fahds2005@icloud.com>
2025-08-12 01:43:54 -0700

    set up security filter chain for oauth2

---
+++ backend/src/main/java/com/fahdsaleh/cleanPR/CleanPrConstants.java
@@ -0,0 +1,8 @@
+package com.fahdsaleh.cleanPR;
+
+public interface CleanPrConstants {
+    String API_PREX = "api/v1/auth";
+    String AUTH_API_PREFIX = API_PREX + "/auth";
+
+
+}
---
+++ backend/src/main/java/com/fahdsaleh/cleanPR/models/Account.java
@@ -0,0 +1,4 @@
+package com.fahdsaleh.cleanPR.models;
+
+public class Account {
+}
