e31609a9131eba6aa2cbd5e142102f815f8a3716
Fahd <fahds2005@icloud.com>
2025-09-21 23:51:30 -0700

    feature: added install/uninstall event handling

--- backend/src/main/java/com/fahd/cleanPR/controller/GitHubWebhooks.java
+++ backend/src/main/java/com/fahd/cleanPR/controller/GitHubWebhooks.java
@@ -1,8 +1,14 @@
 package com.fahd.cleanPR.controller;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.fahd.cleanPR.handler.BaseEventHandler;
+import com.fahd.cleanPR.handler.InstallationEventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.fahd.cleanPR.CleanPrConstants.GITHUB_WEB_HOOK_ROUTE;
 
@@ -11,11 +17,47 @@ import static com.fahd.cleanPR.CleanPrConstants.GITHUB_WEB_HOOK_ROUTE;
 @RequestMapping(GITHUB_WEB_HOOK_ROUTE)
 public class GitHubWebhooks {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(GitHubWebhooks.class);
+
+    private BaseEventHandler baseEventHandler;
+
+    private Map<String, BaseEventHandler> eventDispatcher;
+
+    private InstallationEventHandler installationEventHandler;
+
+
+
+    /**
+     * EventHandlerMapping={
+     *     created or deleted -> installationEventHandler
+     *     repoRemove or repoAdd -> RepoEventHandler
+     *     prOpened or prClosed -> prEventHandler
+     * }
+     * */
+    @Autowired
+    public GitHubWebhooks(
+            final InstallationEventHandler installationEventHandler
+    ) {
+        this.installationEventHandler = installationEventHandler;
+        this.eventDispatcher = Map.of(
+                "created", installationEventHandler,
+                "deleted", installationEventHandler
+        );
+    }
 
     @PostMapping("/github")
-    public void gitHubWebhookEvent(@RequestBody String webHookPayload) {
-        JsonObject jsonObject = JsonParser.parseString(webHookPayload).getAsJsonObject();
-        System.out.println(webHookPayload);
+    public void gitHubWebhookEvent(@RequestBody Map<String, Object> webHookPayload) {
+
+        // mapping the handler based on the action
+        String action = (String) webHookPayload.get("action");
+        baseEventHandler = eventDispatcher.getOrDefault(action, null);
+
+        if (baseEventHandler != null) {
+            baseEventHandler.triggerEvent(webHookPayload, action);
+        } else {
+            LOGGER.info("no handler for action= {}", action);
+        }
+
     }
 
 }
---
+++ backend/src/main/java/com/fahd/cleanPR/repository/InstallationRepository.java
@@ -0,0 +1,7 @@
+package com.fahd.cleanPR.repository;
+
+import com.fahd.cleanPR.model.Installation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InstallationRepository extends JpaRepository<Installation, Integer> {
+}
